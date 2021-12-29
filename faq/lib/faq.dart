14186343016c0f314817336bd0ee373210d3bed5
Siti Fatimah Tuma'ninah <siti.fatimah05@ui.ac.id>
2021-12-29 13:09:12 +0700

    FAQ

--- faq/lib/faq.dart
+++ faq/lib/faq.dart
@@ -1,7 +1,216 @@
-library faq;
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:faq/models/question.dart';
+import 'package:faq/api/api_faq.dart';
+import 'dart:convert';
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+Future<List<Question>?> futureQuestion = fetchQuestion();
+
+void main() => runApp(FaqListScreen());
+
+class FaqListScreen extends StatelessWidget {
+  static const routeName = 'faq/';
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder (
+      future: futureQuestion,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          var data = snapshot.data as List<Question>;
+          List<MyTile> listOfTiles = <MyTile> [
+            for(var q in data)
+              new MyTile(q.fields!.pertanyaan,
+                  <MyTile>[
+                    new MyTile(q.fields!.jawaban)
+                  ])
+          ];
+          return Scaffold(
+            appBar: AppBar(title: Text('Frequently Asked Question'),),
+            body: SingleChildScrollView(
+              physics: ScrollPhysics(),
+              child: Column(
+                children: <Widget>[
+                  Text(""),
+                  Center(
+                    child: Text(
+                      "FAQ",
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(15.0),
+                    itemBuilder: (BuildContext context, int index) {
+                      return new StuffInTiles(listOfTiles[index]);
+                    },
+                    itemCount: listOfTiles.length,),
+                  Center(
+                    child: FormScreen(),
+                  )
+
+                ],
+              ),
+            ),
+          );
+        }
+        return const CircularProgressIndicator();
+      },
+    );
+  }
 }
+
+
+class StuffInTiles extends StatelessWidget {
+  final MyTile myTile;
+  StuffInTiles(this.myTile);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(myTile);
+  }
+
+  Widget _buildTiles(MyTile t) {
+    if (t.children.isEmpty)
+      return new ListTile(
+          dense: true,
+          enabled: true,
+          isThreeLine: false,
+          onLongPress: () => print("long press"),
+          onTap: () => print("tap"),
+          title: new Text(t.title, style: TextStyle(fontSize: 17.0)));
+
+    return new ExpansionTile(
+      key: new PageStorageKey<int>(3),
+      backgroundColor: Colors.white70,
+      collapsedBackgroundColor: Colors.white,
+      collapsedTextColor: Color(0xff0c0c0c),
+      title: new Text(t.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+      children: t.children.map(_buildTiles).toList(),
+    );
+  }
+}
+
+
+class MyTile {
+  String title;
+  List<MyTile> children;
+  MyTile(this.title, [this.children = const <MyTile>[]]);
+}
+
+class FormScreen extends StatelessWidget {
+  String pertanyaan = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        Center(
+          child: Container(
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                  color: Color(0xffc3f1fc),
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              child: new Center (
+                  child: Text(
+                    "Ada Pertanyaan?",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Color(0xff374ABE),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              )
+          ),
+        ),
+
+        SizedBox(height: 20.0),
+
+        new Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(top: 25.0, bottom: 8.0, start: 16.0, end: 16.0),
+            child: Text(
+              "Pertanyaan:",
+              softWrap: true,
+              style: new TextStyle(
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff374ABE)),
+            ),
+          ),
+        ),
+
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  onChanged: (String value) {
+                    pertanyaan = value;
+                  },
+                  maxLines: 7,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Tuliskan pertanyaan Anda...",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.3, color: Colors.blue),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
+
+          ),
+        ),
+
+        SizedBox(height: 20.0),
+        MaterialButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            padding: EdgeInsets.all(15.0),
+
+            color: Color(0xff78d9ea),
+            hoverColor: Color(0xff9decf6),
+
+            onPressed: () async {
+              final response = await http.post(Uri.parse('http://127.0.0.1:8000/Faq/json'),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8'
+                  },
+                  body: jsonEncode(<String, String>{
+                    'pertanyaan': pertanyaan,
+                  }));
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => FaqListScreen()),
+              );
+              futureQuestion = fetchQuestion();
+            },
+            child: Text(" Kirimkan pertanyaan ", style: TextStyle(
+              // fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 17.0
+            ))
+        ),
+        SizedBox(height: 20.0),
+      ],
+    );
+  }
+}
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:faq/models/question.dart';
+import 'package:faq/api/api_faq.dart';
+import 'dart:convert';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future<List<Question>?> futureQuestion = fetchQuestion();
+List<String> list = ["gambar 1","gambar 2","gambar 3"];
+void main() => runApp(FaqListScreen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FaqListScreen extends StatelessWidget {
+  static const routeName = 'faq/';
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hachoo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Hachoo'),
+    return FutureBuilder (
+      future: futureQuestion,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Tidak ada FAQ'),
+          );
+        } else if (snapshot.hasData) {
+          var data = snapshot.data as List<Question>;
+          List<MyTile> listOfTiles = <MyTile> [
+            for(var q in data)
+              new MyTile(q.fields!.pertanyaan ,
+                  <MyTile>[
+                    new MyTile(q.fields!.jawaban)
+                  ])
+          ];
+          return Scaffold(
+            appBar: AppBar(title: Text('Frequently Asked Question'),),
+            body: SingleChildScrollView(
+              physics: ScrollPhysics(),
+              child: Column(
+                children: <Widget>[
+                  Text(""),
+                  Center(
+                    child: Text(
+                      "FAQ",
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(15.0),
+                    itemBuilder: (BuildContext context, int index) {
+                      return new StuffInTiles(listOfTiles[index]);
+                    },
+                    itemCount: listOfTiles.length,),
+                  Center(
+                    child: FormScreen(),
+                  ),
+                  Center(
+                    child: Container(
+                        child: CarouselSlider(
+                          options: CarouselOptions(),
+                          items: list.map((item) => Container(
+                            child: Center(
+                                child: Text(item.toString())
+                            ),
+                            color: Colors.green,
+                          )).toList(),
+                        )
+                    ),
+                  )
+                ],
+              ),
+            ),
+            drawer: NavigationDrawerWidget(),
+          );
+        }
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class StuffInTiles extends StatelessWidget {
+  final MyTile myTile;
+  StuffInTiles(this.myTile);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return _buildTiles(myTile);
+  }
+
+  Widget _buildTiles(MyTile t) {
+    if (t.children.isEmpty)
+      return new ListTile(
+          dense: true,
+          enabled: true,
+          isThreeLine: false,
+          onLongPress: () => print("long press"),
+          onTap: () => print("tap"),
+          title: new Text(t.title, style: TextStyle(fontSize: 17.0)));
+
+    return new ExpansionTile(
+      key: new PageStorageKey<int>(3),
+      backgroundColor: Colors.white70,
+      collapsedBackgroundColor: Colors.white,
+      collapsedTextColor: Color(0xff0c0c0c),
+      title: new Text(t.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+      children: t.children.map(_buildTiles).toList(),
+    );
+  }
+}
+
+
+class MyTile {
+  String title;
+  List<MyTile> children;
+  MyTile(this.title, [this.children = const <MyTile>[]]);
+}
+
+class FormScreen extends StatelessWidget {
+  String pertanyaan = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        Center(
+          child: Container(
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                  color: Color(0xffc3f1fc),
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              child: new Center (
+                  child: Text(
+                    "Ada Pertanyaan?",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Color(0xff374ABE),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+              )
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        SizedBox(height: 20.0),
+
+        new Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(top: 25.0, bottom: 8.0, start: 16.0, end: 16.0),
+            child: Text(
+              "Pertanyaan:",
+              softWrap: true,
+              style: new TextStyle(
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff374ABE)),
+            ),
+          ),
+        ),
+
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  onChanged: (String value) {
+                    pertanyaan = value;
+                  },
+                  maxLines: 7,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Tuliskan pertanyaan Anda...",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.3, color: Colors.blue),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
+
+          ),
+        ),
+
+        SizedBox(height: 20.0),
+        MaterialButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            padding: EdgeInsets.all(15.0),
+
+            color: Color(0xff78d9ea),
+            hoverColor: Color(0xff9decf6),
+
+            onPressed: () async {
+              final response = await http.post(Uri.parse('http://127.0.0.1:8000/Faq/json'),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8'
+                  },
+                  body: jsonEncode(<String, String>{
+                    'pertanyaan': pertanyaan,
+                  }));
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => FaqListScreen()),
+              );
+              futureQuestion = fetchQuestion();
+            },
+            child: Text(" Kirimkan pertanyaan ", style: TextStyle(
+              // fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 17.0
+            ))
+        ),
+        SizedBox(height: 20.0),
+      ],
     );
   }
 }
+
+
+
+
+
+
